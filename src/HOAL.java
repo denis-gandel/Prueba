1f9acea01f8cd95111d721650ef75881cd3ac210
denisganda123 <gandarillas.delgado.denis@gmail.com>
2022-04-23 17:19:10 -0400

    Hola

--- src/HOAL.java
+++ src/HOAL.java
@@ -2,6 +2,8 @@ public class HOAL {
 
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        int suma = 1 + 2;
+        System.out.println(suma);
     }
 
 }
